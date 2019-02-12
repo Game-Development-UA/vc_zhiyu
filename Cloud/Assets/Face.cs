ea9bec2b67d012afb909feaed71c8747ff5a0bca
Zhiyu Long <zhiyulong@email.arizona.edu>
2019-02-11 23:54:19 -0700

    controller_Cloud

---
+++ Cloud/Assets/Face.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Face : MonoBehaviour
+{
+    public float changeVol;
+    public GameObject face;
+    public Rigidbody2D cloud;
+    
+    private void Update()
+    {
+        Cloud rain = cloud.gameObject.GetComponent<Cloud>();
+
+    }
+}
